6a4a080f7ba2131eec176076a1562d49856a2881
{name} <{emailaddress}>
2021-03-17 11:50:04 -0500

    make it better

---
+++ app.js
@@ -0,0 +1,85 @@
+//___dirname = para darle dinamicidad esa va a ser la direccion del hosting
+//usando express npm i express
+    const express = require('express');
+    const app = express();
+
+    const port = 3000;
+
+    //motor de plantillas
+    app.set('view engine', 'ejs');
+    app.set('views',  __dirname + '/views')//nombre de directorio y el folder
+
+
+    app.use(express.static(__dirname + '/public'))
+
+    
+
+
+    app.get('/', (req, res) =>{// '/'= pagina raiz
+        //console.log(__dirname)
+        res.render('index', {titulo: 'mi tutulo dinamico '})
+    })
+
+    app.get('/servicios', (req, res) =>{
+        res.render('servicios', {servicio: 'respuesta desde servicios'})
+    })
+
+    app.use((req, res, next) =>{
+        res.status(404).render('404', {
+            titulo: 404, 
+            descripcion: 'titulo del sitio web'
+        })
+    })//responde cuando no hay una ruta generada
+
+
+
+    app.listen(port, ()=>{
+        console.log('servidor a su servicio en el puerto', port)
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.end('Estoy respondiendo a tu solicitud v.3');
+})
+
+const puerto = 3000;
+server.listen(puerto, () =>{
+    console.log('escuchando solicitudes')
+})
+
+
+ //ctrl + c para terminar con el servidor y volver a poner node app.js
+ // en cmd : nodemon app.js
+// npm i express*/
---
+++ frutas.js
@@ -0,0 +1,13 @@
+const frutas = ['platanos', 'manzana', 'platanos', 'pera'];
+const dinero = 1000;
+
+
+//para exportar mas de uno se usa un objeto {}
+module.exports = {
+    //nombre de la propiedad y el valor !como tiene el mismo nombre se puede usar solo uno
+    frutas : frutas,
+    dinero 
+    
+};
+
+
