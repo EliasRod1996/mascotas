eab9539f72f6433e2f98907bf718dc256f7ac9f2
{name} <{emailaddress}>
2021-03-18 13:27:38 -0500

    cambio3

--- router/RutasWeb.js
+++ router/RutasWeb.js
@@ -6,11 +6,11 @@ const router = express.Router();//con eso se accede a las propuedades del router
 
 router.get('/', (req, res) =>{// '/'= pagina raiz
         //console.log(__dirname)
-        res.render('index', {titulo: 'mi tutulo dinamico '})
+        res.render('index', {titulo: 'mi titulo dinamico '})
     })
  
 router.get('/servicios', (req, res) =>{
-        res.render('servicios', {servicio: 'respuesta desde servicios'})
+        res.render('servicios', {tituloServicio: 'respuesta desde servicios'})
     })
 
 
